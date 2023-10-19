43a98f5b1b0096128233942a83b2197cd2680e54
yelmokht <younes.el.mokhtari@ulb.be>
2023-10-20 00:55:26 +0200

    Fixed Makefile Setup Main.java, need still to fix to display on terminal from jar

--- Makefile
+++ Makefile
@@ -1,9 +1,26 @@
-all:
-	jflex LexicalAnalyzer.flex
-	javac LexicalAnalyzer.java
-	java Main euclid.pmp
+JFLEX = jflex
+JAVAC = javac
+JAVA = java
+JAR = jar
+JAR_NAME = part1.jar
+SRC_DIR = src
+BUILD_DIR = out/production/Compiler
+JAR_DIR = dist
 
-test:
-	jflex LexicalAnalyzer.flex
-	javac LexicalAnalyzer.java
-	java Main ./test/test.txt
+all: compile run
+
+lex: $(SRC_DIR)/LexicalAnalyzer.flex
+	$(JFLEX) $(SRC_DIR)/LexicalAnalyzer.flex
+
+compile: lex
+	$(JAVAC) -d $(BUILD_DIR) -cp $(BUILD_DIR) $(SRC_DIR)/*.java
+
+jar: compile
+	$(JAR) cfe $(JAR_DIR)/$(JAR_NAME) $(SRC_DIR)/Main -C $(BUILD_DIR) .
+
+run:
+	$(JAVA) -cp $(BUILD_DIR) Main $(SRC_DIR)/euclid.pmp
+
+clean:
+	rm $(JAR_DIR)/$(JAR_NAME) $(SRC_DIR)/LexicalAnalyzer.java $(BUILD_DIR)/*
+
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
-import jflex.LexScan;
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-        //Récupérer le fichier en arg + gestion erreur
-        //Lance java LexicalAnalyzer ficher
-        //dans le makefile on fera java Main file
+        try {
+            if (args.length < 1) {
+                throw new IllegalArgumentException("Insufficient arguments. Usage: java MyClass arg1");
+            }
+            String arg1 = args[0];
+            File inputFile = new File(arg1);
+            String[] argv = new String[] {inputFile.getPath()};
+            LexicalAnalyzer.main(argv);
+        } catch (IllegalArgumentException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
     }
 }
