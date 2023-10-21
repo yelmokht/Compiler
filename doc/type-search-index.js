0739c70100a4daba49b95b11d74fbeece6878350
yelmokht <younes.el.mokhtari@ulb.be>
2023-10-21 22:01:26 +0200

    Added JAVADOC

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"LexicalUnit"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Redirect"},{"p":"<Unnamed>","l":"Symbol"},{"p":"<Unnamed>","l":"TestLexicalAnalyzer"}];updateSearchResults();
